37e7dcfbbf143cc6b1c770ce763465c60249c5fb
Elizabeth-Ater <e.ater@alustudent.com>
2026-08-23 11:25:32 +0000

    Updated the file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts.
+1-top_ten
 """
-
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit.
 
-    headers = {
-        "User-Agent": "ALU-Reddit-API"
-    }
+    If not a valid subreddit, prints None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "python3:top.ten.checker:v1.0 (by /u/check)"}
+    params = {"limit": 10}
 
     response = requests.get(
         url,
         headers=headers,
+        params=params,
         allow_redirects=False
     )
 
     if response.status_code != 200:
-        print("None")
+        print(None)
         return
 
-    data = response.json()
-    posts = data.get("data", {}).get("children", [])
+    try:
+        results = response.json().get("data", {}).get("children", [])
+    except ValueError:
+        print(None)
+        return
 
-    for post in posts[:10]:
+    if not results:
+        print(None)
+        return
+
+    for post in results:
         print(post.get("data", {}).get("title"))
